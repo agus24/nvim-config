730a4b71ab8e8724e2abc4b20760a14e147c4f53
gustiawan <agusx244@gmail.com>
2025-03-18 15:11:46 +0700

    update keymap

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -2,7 +2,8 @@
 vim.keymap.set('n', 'H', '<Cmd>BufferPrevious<CR>', { desc = 'Move to previous buffer' })
 vim.keymap.set('n', 'L', '<Cmd>BufferNext<CR>', { desc = 'Move to next buffer' })
 -- vim.keymap.set('n', '<Leader>bd', '<Cmd>BufferClose<CR>', { desc = 'Close current buffer' })
-vim.keymap.set('n', '<C-q>', '<Cmd>BufferClose<CR>', { desc = 'Close current buffer' })
+vim.keymap.set('n', '<Leader>bd', '<Cmd>BufferClose<CR>', { desc = 'Close current buffer' })
+vim.keymap.set('v', '<leader>p', '"0p', { noremap = true, silent = true, desc = 'Paste last yanked text' })
 
 -- scratch
 vim.keymap.set('n', '<Leader>.', '<cmd>Scratch<cr>', { desc = 'Create new scratch' })
